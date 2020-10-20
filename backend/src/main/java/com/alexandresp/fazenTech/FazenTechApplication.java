fbcd8b222a6e7a6b7591dfe71756f1ccd44be4cb
Alexandre Pereira <alexandresp15@gmail.com>
2020-10-20 14:52:58 -0300

    Project created

---
+++ backend/src/main/java/com/alexandresp/fazenTech/FazenTechApplication.java
@@ -0,0 +1,13 @@
+package com.alexandresp.fazenTech;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FazenTechApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FazenTechApplication.class, args);
+	}
+
+}
---
+++ backend/src/test/java/com/alexandresp/fazenTech/FazenTechApplicationTests.java
@@ -0,0 +1,13 @@
+package com.alexandresp.fazenTech;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FazenTechApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
